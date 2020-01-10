4cdb2f709efef66b7056981d9c8d4e4d5d6e9d9d
Vinícius <vinicius.matheus252@gmail.com>
2020-01-10 00:45:53 -0300

    Add Response parser

--- src/response.py
+++ src/response.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from src.models.work import Work
+from src.models.aggregation import Aggregation
 
 
 class Response:
@@ -14,8 +16,18 @@ class Response:
 
             Attr:
                 response_json (dict): resposta da requisição
+                page (int): número da página
+                page_size (int): tamanho da página
+                total (int): número total de trabalhos do resultado da requisição
+                works (list): lista de trabalhos
+                aggregations (list): lista de agregações
         '''
         self.response_json = response_json
+        self.page = 0
+        self.page_size = 0
+        self.total = 0
+        self.works = []
+        self.aggregations = []
 
         self.__parse_response()
 
@@ -23,4 +35,14 @@ class Response:
         '''
             Realiza o parse da resposta da requisição para os modelos
         '''
-        return
+
+        self.page = self.response_json.get('pagina', self.page)
+        self.page_size = self.response_json.get(
+            'registrosPorPagina', self.page_size)
+        self.total = self.response_json.get('total', self.total)
+
+        for work in self.response_json.get('tesesDissertacoes', self.works):
+            self.works.append(Work().parse(work))
+
+        for aggregation in self.response_json.get('agregacoes', self.aggregations):
+            self.aggregations.append(Aggregation.parse(aggregation))
