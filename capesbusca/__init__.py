7e011f04f0a6793cf85b0f07b8cbc591d3d358b6
Vinícius <vinicius.matheus252@gmail.com>
2020-01-11 18:52:44 -0300

    Add search as a function

--- capesbusca/__init__.py
+++ capesbusca/__init__.py
@@ -1,4 +1,4 @@
 from capesbusca.searchquery import SearchQuery
 from capesbusca.searchresult import SearchResult
 from capesbusca.searcherror import SearchError
-from capesbusca.request import Request
+from capesbusca.request import search
--- capesbusca/request.py
+++ capesbusca/request.py
@@ -12,61 +12,58 @@ AGGREGATIONS_URL = SEARCH_URL + \
     "/agregacoes?termo={}&agregado={}&pagina={}&registrosPorPagina={}"
 
 
-class Request:
+def search(search_query, full_aggregations=False):
+    '''
+        Realiza a requisição de busca
 
-    ''' Classe para realizar os requisições dos dados '''
+        Params:
+            search_query (search_query.SearchQuery): Query de busca
+            full_aggregations (bool): Flag para realizar a requisição da lista completa de agregações
 
-    def search(self, search_query, full_aggregations=False):
-        '''
-            Realiza a requisição de busca
+        Returns:
+            (searchresult.SearchResult): Resultado da busca
+    '''
 
-            Params:
-                search_query (search_query.SearchQuery): Query de busca
-                full_aggregations (bool): Flag para realizar a requisição da lista completa de agregações
+    response_json = {}
 
-            Returns:
-                (searchresult.SearchResult): Resultado da busca
-        '''
+    try:
+        response_json = requests.post(SEARCH_URL, data=search_query.json(),
+                                      headers={'content-type': 'application/json'}).json()
 
-        response_json = {}
+        if full_aggregations:
+            response_json['agregacoes'] = get_full_aggregations(
+                search_query, response_json.get('agregacoes', []))
+
+    except json.decoder.JSONDecodeError:
+        raise SearchError("Error doing the search")
+    except ConnectionError:
+        raise SearchError("Connection error")
+
+    return SearchResult(response_json)
+
+
+def get_full_aggregations(search_query, initial_aggregations):
+
+    aggregations = deepcopy(initial_aggregations)
+
+    for aggregation in aggregations:
+        aggregation_name = aggregation.get('campo', '') + '_raw'
+
+        url = AGGREGATIONS_URL.format(
+            quote(search_query.term), quote(aggregation_name), search_query.page, search_query.page_size)
 
         try:
-            response_json = requests.post(SEARCH_URL, data=search_query.json(),
-                                          headers={'content-type': 'application/json'}).json()
+            response = requests.post(url, data=search_query.json(), headers={
+                'content-type': 'application/json'}, timeout=5).json()
 
-            if full_aggregations:
-                response_json['agregacoes'] = self.__get_full_aggregations(
-                    search_query, response_json.get('agregacoes', []))
+            if response.get('total', 0) > 0:
+                aggregation['agregados'] = response['agregados']
 
+        except requests.exceptions.ReadTimeout:
+            pass
         except json.decoder.JSONDecodeError:
-            raise SearchError("Error doing the search")
+            pass
         except ConnectionError:
-            raise SearchError("Connection error")
+            pass
 
-        return SearchResult(response_json)
-
-    def __get_full_aggregations(self, search_query, initial_aggregations):
-
-        aggregations = deepcopy(initial_aggregations)
-
-        for aggregation in aggregations:
-            aggregation_name = aggregation.get('campo', '') + '_raw'
-
-            url = AGGREGATIONS_URL.format(
-                quote(search_query.term), quote(aggregation_name), search_query.page, search_query.page_size)
-
-            try:
-                response = requests.post(url, data=search_query.json(), headers={
-                    'content-type': 'application/json'}, timeout=5).json()
-
-                if response.get('total', 0) > 0:
-                    aggregation['agregados'] = response['agregados']
-
-            except requests.exceptions.ReadTimeout:
-                pass
-            except json.decoder.JSONDecodeError:
-                pass
-            except ConnectionError:
-                pass
-
-        return aggregations
+    return aggregations
